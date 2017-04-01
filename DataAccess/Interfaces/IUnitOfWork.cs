8b5546e99346343129f10c22f864950e9a4c950f
Grivladan <grigorenkovlad1993@gmail.com>
2017-04-01 21:42:50 +0300

    add IUnitOfWork interface

--- DataAccess/Interfaces/IUnitOfWork.cs
+++ DataAccess/Interfaces/IUnitOfWork.cs
@@ -1,6 +1,18 @@
-﻿namespace DataAccess.Interfaces
+﻿using DataAccess.Entities;
+using System;
+
+namespace DataAccess.Interfaces
 {
-    interface IUnitOfWork
+    public interface IUnitOfWork : IDisposable
     {
+        IRepository<Business> Businesses { get; }
+        IRepository<Country> Countries { get; }
+        IRepository<Department> Departments { get; }
+        IRepository<Family> Families { get; }
+        IRepository<Offering> Offerings { get; }
+        IRepository<Organization> Organizations { get; }
+        IRepository<User> Users { get; }
+
+        void Save();
     }
 }
